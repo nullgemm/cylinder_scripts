f608664e30f7241b3d70923b3b30e1d5d6ce18c2
nullgemm <nullgemm@mailbox.org>
2017-01-24 07:52:14 +0100

    clean up and formatting

--- StarWars.lua
+++ StarWars.lua
@@ -1,23 +1,24 @@
--- StarWars animation by Cylgom
 return function(view, offset, screen_width, screen_height)
-        side = -1
-        if(offset>0) then side=1 end
+   local percent = math.abs(offset/view.width)
+   
+   local side = -1
+   if(offset > 0) then side = 1 end
 
-        local percent = math.abs(offset/view.width)
-        local rollup = percent*5
-        local runaway = 0
-        if(rollup>1) then rollup=1; runaway=(percent-0.2)*5/3 end
-        if(runaway>1) then runaway=1; rollup=1-(percent-0.8)*5 end
+   local rollup = percent*5
+   local runaway = 0
+   if(rollup > 1)  then rollup  = 1; runaway = (percent-0.2) * 5/3 end
+   if(runaway > 1) then runaway = 1; rollup  = 1 - (percent-0.8) * 5 end
 
-          local angle = rollup*math.pi/3
-          local offsetz = math.sin(angle)*(view.height/2)
-          local offsety = math.cos(angle)*(view.height/2)
+   local angle = rollup * math.pi/3
+   local offsetz = math.sin(angle) * (view.height/2)
+   local offsety = math.cos(angle) * (view.height/2)
 
-        for i, icon in subviews(view) do       
-          icon:translate(0,side*(view.height+20)*runaway, 0)
-        end
-          view.layer.y = view.height/2+(view.height/2)-offsety
-          view.layer.z= -offsetz
-          view.layer.x= view.width/2+view.x+offset
-          view:rotate(angle,1,0,0)
+   for i, icon in subviews(view) do       
+      icon:translate(0, side * (view.height+20) * runaway, 0)
+   end
+   
+   view.layer.y =  view.height/2 + (view.height/2) - offsety
+   view.layer.z = -offsetz
+   view.layer.x =  view.width/2 + view.x + offset
+   view:rotate(angle, 1, 0, 0)
 end
--- Waterspiral.lua
+++ Waterspiral.lua
@@ -1,34 +1,35 @@
--- Waterspiral animation by Cylgom
 return function(page, offset, screen_width, screen_height)
-	local percent = math.abs(offset/page.width)
-	side = -1
-	if(offset>0) then side=1 end
+   local percent = math.abs(offset/page.width)
+   side = -1
+   if(offset > 0) then side = 1 end
 
-	local rollup = percent*5
-	if(rollup>1) then rollup=1 end
-	local runaway = (percent-0.20)/0.6
-	if(runaway<0) then runaway=0 end
-	if(runaway>1) then runaway=1 end
+   local rollup = percent * 5
+   if(rollup > 1) then rollup = 1 end
 
-	local middleX =page.width/2
-	local middleY =page.height/2+7
-	local radiusparts = (middleY)/#page.subviews
-	local theta = (3.5*math.pi)/#page.subviews
+   local runaway = (percent-0.20)/0.6
+   if(runaway < 0) then runaway = 0 end
+   if(runaway > 1) then runaway = 1 end
 
-	for i, icon in subviews(page) do
-	  local initangle =(i-1)*theta
-	  local initradius =(i-1)*radiusparts
-	  local angle = initangle+runaway*(3.5*math.pi-initangle)
-	  local radius = initradius+runaway*(middleY-initradius)+45
-	  local iconX = icon.x+icon.width/2
-	  local iconY = icon.y+icon.height/2
-	  local size = 1-(1-(radius/(middleY+50)))*rollup
-	  local pathX = (middleX+radius*(math.cos(angle)+math.sin(10*angle)/15)*side)
-	  local pathY = (middleY+radius*(math.sin(angle)+math.cos(10*angle)/15)*side)
-	  local iconAngle = angle
-	  icon:translate(rollup*(pathX-iconX), rollup*(pathY-iconY), 0)
-	  icon:rotate(rollup*(iconAngle+math.cos(10*angle)*(math.pi/4)))
-	  icon:scale(size*size)
-	end
-	page:translate(offset,0,0)
+   local middleX = page.width/2
+   local middleY = page.height/2 + 7
+   local radiusparts = (middleY)/#page.subviews
+   local theta = (3.5*math.pi)/#page.subviews
+
+   for i, icon in subviews(page) do
+      local initangle =(i-1) * theta
+      local initradius =(i-1) * radiusparts
+      local angle = initangle + runaway * (3.5*math.pi-initangle)
+      local radius = initradius + runaway * (middleY-initradius) + 45
+      local iconX = icon.x + icon.width/2
+      local iconY = icon.y + icon.height/2
+      local pathX = (middleX + radius * (math.cos(angle) + math.sin(10*angle)/15) * side)
+      local pathY = (middleY + radius * (math.sin(angle) + math.cos(10*angle)/15) * side)
+      local size = 1 - (1-(radius/(middleY+50))) * rollup
+
+      icon:translate(rollup * (pathX-iconX), rollup * (pathY-iconY), 0)
+      icon:rotate(rollup*(angle + math.cos(10*angle) * (math.pi/4)))
+      icon:scale(size * size)
+   end
+   
+   page:translate(offset, 0, 0)
 end
